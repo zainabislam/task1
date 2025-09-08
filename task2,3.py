53183f23a4232f44382c711f6750081a7643c86d
zainabislam <candy.mileshehe@gmail.com>
2025-09-08 22:03:27 +0300

    Add files via upload

---
+++ task2,3.py
@@ -0,0 +1,29 @@
+task2,3
+the_list = ["student_1", "student_2", "student_3"]
+student_1= {
+    "name": "sara",
+    "science": 90,
+    "english": 95,
+    "maths": 85,
+    "av.garde": "A"
+    }
+student_2= {
+    "name": "malak",
+    "science": 70,
+    "english": 88,
+    "maths": 90,
+    "av.grade": "C"
+    }
+student_3={
+    "name": "youssef",
+    "science" : 95,
+    "english": 90,
+    "maths": 70,
+    "av.grade": "B"
+    }
+print(student_1["name"])
+print(student_1["av.garde"])
+print(student_2["name"])
+print(student_2["av.garde"])
+print(student_3["name"])
+print(student_3["av.garde"])
